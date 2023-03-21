893ca31b085c0c17c1aca5220fe946730d59c4f2
Lucas Kaua Dias Cunha <lucasdcunha42@gmail.com>
2023-03-21 18:08:56 -0300

    main js functionality

---
+++ main.js
@@ -0,0 +1,23 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play() ;
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //template
+    const idAudio = `#som_${instrumento}`;
+
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom(idAudio);
+    };
+
+    contador = contador + 1;
+    console.log(contador);
+}
